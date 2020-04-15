4c36728c8a6c62c68c1ec66ab4ac0e2f5df21e84
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-15 14:26:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,13 +29,12 @@ const fi = (function() {
 
     find: function(collection, predicate){
       const newCollection = (collection instanceof Array) ? [...collection] : Object.values(collection)
-      for(const i of newCollection){ callback(i)}
       let returnTrue = function(i){
         if(i === predicate){
           return i
         }
       }
-      collection.each(collection, returnTrue)
+      for(const i of newCollection){ callback(i)}
     },
 
     functions: function() {
