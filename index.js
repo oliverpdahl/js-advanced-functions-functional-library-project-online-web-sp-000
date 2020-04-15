995bda33e84b89a7995e9eaa9e9debc58fa86be0
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-15 14:24:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const fi = (function() {
 
     find: function(collection, predicate){
       let returnTrue = function(i){
-        if i === predicate{
+        if(i === predicate){
           return i
         }
       }
