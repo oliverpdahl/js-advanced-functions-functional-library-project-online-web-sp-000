e26fc0717d596593c0ac77977bf2eaf32990ef69
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-15 15:19:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ const fi = (function() {
 
     flatten: function(array, shallow){
       const newCollection = (array instanceof Array) ? [...array] : Object.values(array)
-      newArr = []
+      let newArr = []
       if(shallow){
         for(const i of array){
           if(i instanceof Array){
